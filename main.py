495edf6915af06b98fc3be27c4f95800d919bd30
Tushar <ertusharchoudhary08@gmail.com>
2025-05-29 02:29:18 +0530

    update cors policy

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI, APIRouter
+from fastapi.middleware.cors import CORSMiddleware
 from routes.admin import admin_router
 
 app = FastAPI()
+
+# Allow CORS for React dev server
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 main_router = APIRouter()
 
 @main_router.get("/health")
